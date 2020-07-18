38a5680d8ee16bee30a691380856e40930fb0638
YPN078 <YPN078@yunokawapn.co.jp>
2020-07-18 09:06:39 +0900

    index page title updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -244,3 +244,12 @@
 			._parallax();
 
 })(jQuery);
+
+
+/****  ****/
+
+$('.text').html(function(i, html) {
+	var chars = $.trim(html).split("");
+  
+	return '<span>' + chars.join('</span><span>') + '</span>';
+  });
